c857657893231f4cb43d84e0d61f900fee8be085
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-18 18:02:50 +0530

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { in: 1..15 }
+  validates :surname, presence: true, length: { in: 1..15 }
 end
