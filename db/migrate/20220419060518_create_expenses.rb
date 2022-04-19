e597b88c2b50d6261c38f673593015e9abf3b5de
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-19 11:43:50 +0530

    Create schema

--- db/migrate/20220419060518_create_expenses.rb
+++ db/migrate/20220419060518_create_expenses.rb
@@ -2,7 +2,7 @@ class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
       t.references :user, null: false, foreign_key: { to_table: 'users'}, index: true
-      t.name :name
+      t.string :name
       t.decimal :amount
       t.timestamps
     end
