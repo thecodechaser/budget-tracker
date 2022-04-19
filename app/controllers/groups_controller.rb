91becc90cc2e380fba245a62afebef3ca252c721
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-19 23:44:42 +0530

    Add methoos in controller to crreate new group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,6 +9,17 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @new_group = current_user.groups.new(group_params)
+    if @new_group.save
+      redirect_to groups_path, flash: { alert: 'Your category is registered' }
+    else
+      redirect_to new_group_path, flash: { alert: 'Could not register your category' }
+    end
+  end
 
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
