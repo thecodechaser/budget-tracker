a6de3f5673445492ede07c030d7b5a26ae692394
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-20 01:04:39 +0530

    Add transaction index view

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,10 @@
 class ExpensesController < ApplicationController
 
   def index
-
+    @group = Group.find(params[:group_id])
+    @expenses = [] 
+    @group.group_expenses.each do |group_expense|
+      @expenses.push(group_expense.expense)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :groups, only: [:index, :new, :create]
+  resources :groups, only: [:index, :new, :create] do
+    resources :expenses, only: [:index, :new, :create]
+  end
 
   # Defines the root path route ("/")
   root "home_page#index"
