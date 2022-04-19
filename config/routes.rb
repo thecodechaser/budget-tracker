4e954af79c88dea5cc3271e0b05bbc815162c97d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-19 23:05:34 +0530

    Add routes for groups

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :groups, only: [:index]
+
   # Defines the root path route ("/")
   root "home_page#index"
+
 end
