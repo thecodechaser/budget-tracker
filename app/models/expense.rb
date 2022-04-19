1bd2a763943322846ce75870fde0e3a700c9a704
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-19 11:56:32 +0530

    Setup associations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user
+  has_many :group_expenses, dependent: :destroy
+  has_many :groups, through: :group_expenses
   
   end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
-belongs_to :user
+  belongs_to :user
+  has_many :group_expenses, dependent: :destroy
+  has_many :expenses, through: :group_expenses
 
 end
