a58ec36ff290e5006916dd3ec2618fafeef3b10d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-20 13:31:10 +0530

    Add tests for group index view

---
+++ spec/views/groups/groups_index_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'groups#index', type: :feature do
+  describe 'Group' do
+    before(:each) do
+      @user = User.create(name: 'Ranjeet', surname: 'Singh', email: 'amy@gmail.com', password: 'password')
+      visit new_user_session_path
+      fill_in 'Email', with: 'amy@gmail.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+
+      @group1 = Group.new(user: @user, name: 'Cars', icon: 'icon-link')
+      @group2 = Group.new(user: @user, name: 'Foods', icon: 'icon-link')
+      @group3 = Group.new(user: @user, name: 'Drinks', icon: 'icon-link')
+      @group4 = Group.new(user: @user, name: 'Grocery', icon: 'icon-link')
+
+      visit(groups_path)
+    end
+
+    it 'shows the group name' do
+      expect(page).to have_content('TRANSACTIONS')
+    end
+  end
+end
